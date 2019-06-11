d9482ba41c50b0ff0aa9cb7b9a95539b48628247
maomaochongzu <49610343+maomaochongzu@users.noreply.github.com>
2019-06-11 10:38:00 +0800

    Add files via upload

---
+++ 已完成代码/SysManage/frmDataRevert.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace SMS.SysManage
+{
+    public partial class frmDataRevert : Form
+    {
+        public frmDataRevert()
+        {
+            InitializeComponent();
+        }
+
+        SMS.BaseClass.DataCon datacon = new SMS.BaseClass.DataCon();
+        private void btnSel_Click(object sender, EventArgs e)
+        {
+            ofDialogFile.InitialDirectory = "D:\\";
+            ofDialogFile.Filter = "bak files (*.bak)|*.bak";
+            ofDialogFile.RestoreDirectory = true;
+            ofDialogFile.ShowDialog();
+            txtDRPath.Text = ofDialogFile.FileName.ToString().Trim();
+        }
+
+        private void btnDRevert_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtDRPath.Text.Trim()))
+            {
+                MessageBox.Show("请选择还原路径！");
+                return;
+            }
+            try
+            {
+                datacon.getcom("use master restore database db_SMS from disk='" + txtDRPath.Text.Trim() + "'");
+                MessageBox.Show("数据还原成功！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmDataRevert_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
